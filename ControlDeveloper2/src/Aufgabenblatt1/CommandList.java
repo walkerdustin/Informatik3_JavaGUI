404acdb0967b90367b4a4ca6a0adba461404127f
Dustin_dusTir <du-wal@web.de>
2019-04-12 13:22:38 +0200

    Implementierung der CommandLiszt Klasse durch Linked List Klasse Ohne Element Klasse.

--- ControlDeveloper2/src/Aufgabenblatt1/CommandList.java
+++ ControlDeveloper2/src/Aufgabenblatt1/CommandList.java
@@ -1,11 +1,46 @@
 package Aufgabenblatt1;
 
+import java.util.LinkedList;
 
+import hsrt.mec.controldeveloper.core.com.command.ICommand;
 
 public class CommandList  {
 	
+	private LinkedList<ICommand> commandList  = new LinkedList<ICommand>();
 	
+	public boolean add(ICommand c) {
+		
+		commandList.add(c);
+		return true;
+	}
+	
+	public boolean remove (int pos) {
+		
+		ICommand o = commandList.remove(pos);
+		
+		return o != null;
+	}
 
+	public ICommand get (int pos) {
+		//Icommand c = commandList.get(pos);
+		return commandList.get(pos);
+	}
+	
+	public boolean moveUp(int pos) {
+		if(pos >= commandList.size()) {return false;}
+		
+		ICommand c =commandList.remove(pos);
+		commandList.add(pos - 1, c);
+		
+		return true;
+	}
 
-
+	public boolean moveDown(int pos) {
+		if(pos >= commandList.size()) {return false;}
+		
+		ICommand c =commandList.remove(pos);
+		commandList.add(pos + 1, c);
+		
+		return true;
+	}
 }
