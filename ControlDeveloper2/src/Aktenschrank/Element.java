c1bb79ca6116692b63fd830127e72100292aee5b
TheRealTripleM <TheRealTripleM@DESKTOP-E0MPD4U.speedport.ip>
2019-04-15 15:04:33 +0200

    Aktenschrank angelegt
    - Datein für Verkettete Liste verschoben

---
+++ ControlDeveloper2/src/Aktenschrank/Element.java
@@ -0,0 +1,18 @@
+package Aktenschrank;
+
+import hsrt.mec.controldeveloper.core.com.command.ICommand;
+
+public class Element {
+	private Element next;
+	private Element prev;
+	private ICommand element;
+	
+	public Element(ICommand element){
+		this.element = element;
+		
+	}
+	public ICommand getElement() {
+		return this.element;
+	}
+	
+}
