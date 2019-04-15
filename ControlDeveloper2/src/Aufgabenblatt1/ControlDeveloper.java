f8643260f998fda5eb49058c74c524fa4861deb1
TheRealTripleM <marco.mader_mendes@student.reutlingen-university.de>
2019-04-15 17:34:13 +0200

    Test

--- ControlDeveloper2/src/Aufgabenblatt1/ControlDeveloper.java
+++ ControlDeveloper2/src/Aufgabenblatt1/ControlDeveloper.java
@@ -100,7 +100,7 @@ public class ControlDeveloper {
 		ControlDeveloper cD = new ControlDeveloper();
 		
 		System.out.println(ControlDeveloper.name);
-		System.out.println("TestAlpha  und Test Delta");
+		System.out.println("TestAlpha  und Test Echo");
 		// Testkomment
 		System.out.println(ControlDeveloper.getName());
 		
