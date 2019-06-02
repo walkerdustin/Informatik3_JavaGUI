a3a5fb461170169031b7e9ff99d47c751848822d
Dustin_dusTir <du-wal@web.de>
2019-06-02 21:44:00 +0200

    VerbesserungsVorschlag @ Marco

--- ControlDeveloper2/src/GUI/PanelTypesView.java
+++ ControlDeveloper2/src/GUI/PanelTypesView.java
@@ -1,6 +1,5 @@
 package GUI;
 
-
 import java.awt.BorderLayout;
 import java.util.Vector;
 
@@ -14,69 +13,62 @@ import javax.swing.ScrollPaneConstants;
 import Controller.ControlDevelepor;
 import Model.ControlModel;
 
-public class PanelTypesView extends JPanel implements iGui{
+public class PanelTypesView extends JPanel implements iGui { // by Dustin ... PTV muss iGui eigentlich nicht
+																// implementieren, da der Content statisch ist?!
 	private ControlDevelepor cD;
 	private static PanelTypesView pTV = null;
 	private static GUIList TypesList = null;
-	
+
 	private static final String HEADLINE = "Types";
 	private String[] arrCommandTypes;
 	private AddButton bAdd;
-	
-	private  PanelTypesView(ControlDevelepor cD, String[] arrList) {
+
+	private PanelTypesView(ControlDevelepor cD, String[] arrList) {
 
 		System.out.println("ConstruktorPanel");
 		this.cD = cD;
 		arrCommandTypes = arrList;
-		
+
 		setLayout(new BorderLayout());
 		add(new JLabel(HEADLINE, JLabel.CENTER), BorderLayout.NORTH);
 		setSize(200, 400);
 
-		
-		
-		System.out.println("PanelAngelegt");		
+		System.out.println("PanelAngelegt");
 		testList();
-		
-		
-		
+
 		bAdd = new AddButton(cD);
 		add(bAdd, BorderLayout.SOUTH);
-		
-		
-	}
-	
 
-	
-	static public PanelTypesView getTypesView (ControlDevelepor cD,String[] arrList) {
+	}
+
+	static public PanelTypesView getTypesView(ControlDevelepor cD, String[] arrList) {
 		if (pTV == null) {
-			pTV = new PanelTypesView(cD,arrList);
+			pTV = new PanelTypesView(cD, arrList);
 		}
-		
+
 		System.out.println("Panel übergeben");
 		return pTV;
 
 	}
-	
-	void testList()  {
-		
-		TypesList = new GUIList(arrCommandTypes);
-		add(new JScrollPane(TypesList,  ScrollPaneConstants.VERTICAL_SCROLLBAR_AS_NEEDED,ScrollPaneConstants.HORIZONTAL_SCROLLBAR_AS_NEEDED), BorderLayout.CENTER);
-		//TypesList.addElement("Ameisenbär");
-		
-	}
 
+	void testList() {
+
+		TypesList = new GUIList(arrCommandTypes);
+		add(new JScrollPane(TypesList, ScrollPaneConstants.VERTICAL_SCROLLBAR_AS_NEEDED,
+				ScrollPaneConstants.HORIZONTAL_SCROLLBAR_AS_NEEDED), BorderLayout.CENTER);
+		// TypesList.addElement("Ameisenbär");
+
+	}
 
 	@Override
 	public void updateView() {
 		// TODO Auto-generated method stub
 		TypesList.add("TestString");
+
 	}
 
-
-
 	public String getSelectedType() {
-		
+
 		return (String) TypesList.getSelectedValue();
 	}
 
